944fb804fa5e7e2812c9fb3ff8acc23fbef9aaa0
Gabriel <gabyiy2000@hotmail.com>
2022-04-13 22:34:11 +0300

    next

--- src/components/SelectedCity.js
+++ src/components/SelectedCity.js
@@ -49,26 +49,26 @@ props.getImgNStatic()
 
 const imaine= `http://openweathermap.org/img/wn/${icon}.png`
 
-  if (imageTime === "Clouds" && hh>=8 && hh<=21  ) {
+  if (imageTime === "Clouds" && hh>=8 && hh<=20  ) {
     props.getImgNStatic("url(/images/clouds.avif)")
 
   }else if  (imageTime === "Clouds") {
     props.getImgNStatic("url(/images/nigthClouds.jpg)")
 
-  }else if (imageTime === "Mist" && hh>=8 && hh<=21) {
+  }else if (imageTime === "Mist" && hh>=8 && hh<=20) {
     props.getImgNStatic("url(/images/mist.webp)")
   }else if  (imageTime === "Mist") {
     props.getImgNStatic("url(/images/nigthMist.jpg)")
 
   }
-   else if (imageTime === "Snow" && hh>=8 && hh<=21) {
+   else if (imageTime === "Snow" && hh>=8 && hh<=20) {
     props.getImgNStatic("url(/images/snow.jpg)")
 
   }else if (imageTime === "Snow") {
     props.getImgNStatic("url(/images/nigthSnow.jpg)")
 
   }
-   else if (imageTime === "Rain"  ||imageTime ==="Drizzle" && hh>=8 && hh<=21 ) {
+   else if (imageTime === "Rain"  ||imageTime ==="Drizzle" && hh>=8 && hh<=20 ) {
     props.getImgNStatic("url(/images/rain.jpg)")
   } else if (imageTime === "Rain"  ||imageTime ==="Drizzle") {
     props.getImgNStatic("url(/images/rainy-night.jpg)")
@@ -77,7 +77,7 @@ const imaine= `http://openweathermap.org/img/wn/${icon}.png`
   
     props.getImgNStatic("url(/images/clearSkyDay.jpg)")
   
-} else if (imageTime==="Clear" && hh>=8 && hh<=21){
+} else if (imageTime==="Clear" && hh>=8 && hh<=20){
   
   props.getImgNStatic("url(/images/clearSkyNigth.jpg)")
 
